0ea8f19406ba5f8d92e7a77afa345fa5eb86238d
Stephan <stephanf01@gmail.com>
2021-08-08 22:40:09 +0000

    Unit Tests Running

---
+++ UnitTest/CachingFactoryTest.cpp
@@ -0,0 +1,265 @@
+
+#include <array>
+#include <catch2/catch_all.hpp>
+#include <chrono>
+#include <thread>
+
+#include "../CorkLib/Util/CachingFactory.h"
+#include "../CorkLib/Util/Resettable.h"
+
+//  The pragma below is to disable to false errors flagged by intellisense for Catch2 REQUIRE macros.
+
+#if __INTELLISENSE__
+#pragma diag_suppress 2486
+#endif
+
+class NonResettableObject
+{
+   public:
+    NonResettableObject() : m_value(0) {}
+
+    ~NonResettableObject() {}
+
+    long getValue() const { return (m_value); }
+
+    void setValue(long value) { m_value = value; }
+
+   private:
+    long m_value;
+};
+
+class ResettableObject : SEFUtility::Resettable
+{
+   public:
+    ResettableObject() : m_value(0) {}
+
+    ~ResettableObject() {}
+
+    void reset() { m_value = 0; }
+
+    long getValue() const { return (m_value); }
+
+    void setValue(long value) { m_value = value; }
+
+   private:
+    long m_value;
+};
+
+TEST_CASE("Test Non Resetting Caching Factory", "[cork-base]")
+{
+    SEFUtility::CachingFactory<NonResettableObject> nonResettableFactory;
+
+    REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 0);
+
+    {
+        SEFUtility::CachingFactory<NonResettableObject>::UniquePtr object1 =
+            nonResettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::CACHE);
+
+        REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 0);
+
+        object1->setValue(1);
+    }
+
+    REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 1);
+
+    {
+        SEFUtility::CachingFactory<NonResettableObject>::UniquePtr object1 =
+            nonResettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::CACHE);
+
+        REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 0);
+        REQUIRE(object1->getValue() == 1);
+
+        SEFUtility::CachingFactory<NonResettableObject>::UniquePtr object2 =
+            nonResettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::CACHE);
+
+        REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 0);
+        REQUIRE(object2->getValue() == 0);
+
+        object2->setValue(2);
+    }
+
+    REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 2);
+
+    {
+        SEFUtility::CachingFactory<NonResettableObject>::UniquePtr object1 =
+            nonResettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::DESTROY);
+
+        REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 1);
+        REQUIRE(object1->getValue() == 2);
+
+        SEFUtility::CachingFactory<NonResettableObject>::UniquePtr object2 =
+            nonResettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::CACHE);
+
+        REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 0);
+        REQUIRE(object2->getValue() == 1);
+
+        SEFUtility::CachingFactory<NonResettableObject>::UniquePtr object3 =
+            nonResettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::CACHE);
+
+        REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 0);
+        REQUIRE(object3->getValue() == 0);
+
+        object3->setValue(3);
+    }
+
+    REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 2);
+
+    SEFUtility::CachingFactory<NonResettableObject>::clear();
+
+    REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 0);
+}
+
+TEST_CASE("Test Resetting Caching Factory", "[cork-base]")
+{
+    SEFUtility::CachingFactory<ResettableObject> resettableFactory;
+
+    REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 0);
+
+    {
+        SEFUtility::CachingFactory<ResettableObject>::UniquePtr object1 =
+            resettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::CACHE);
+
+        REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 0);
+
+        object1->setValue(1);
+    }
+
+    REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 1);
+
+    {
+        SEFUtility::CachingFactory<ResettableObject>::UniquePtr object1 =
+            resettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::CACHE);
+
+        REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 0);
+        REQUIRE(object1->getValue() == 0);
+
+        SEFUtility::CachingFactory<ResettableObject>::UniquePtr object2 =
+            resettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::CACHE);
+
+        REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 0);
+        REQUIRE(object2->getValue() == 0);
+
+        object2->setValue(2);
+    }
+
+    REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 2);
+
+    {
+        SEFUtility::CachingFactory<ResettableObject>::UniquePtr object1 =
+            resettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::DESTROY);
+
+        REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 1);
+        REQUIRE(object1->getValue() == 0);
+
+        SEFUtility::CachingFactory<ResettableObject>::UniquePtr object2 =
+            resettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::CACHE);
+
+        REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 0);
+        REQUIRE(object2->getValue() == 0);
+
+        SEFUtility::CachingFactory<ResettableObject>::UniquePtr object3 =
+            resettableFactory.GetInstance(SEFUtility::CachingFactoryCacheOrDestroy::CACHE);
+
+        REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 0);
+        REQUIRE(object3->getValue() == 0);
+
+        object3->setValue(3);
+    }
+
+    REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 2);
+
+    SEFUtility::CachingFactory<ResettableObject>::clear();
+
+    REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 0);
+}
+
+void NonResettableThreadMain(SEFUtility::CachingFactoryCacheOrDestroy cacheOrDestroy, long numLoops)
+{
+    SEFUtility::CachingFactory<NonResettableObject> factory;
+
+    for (int i = 0; i < numLoops; i++)
+    {
+        SEFUtility::CachingFactory<NonResettableObject>::UniquePtr object = factory.GetInstance(cacheOrDestroy);
+
+        object->setValue(object->getValue() + 1);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+    }
+}
+
+void ResettableThreadMain(SEFUtility::CachingFactoryCacheOrDestroy cacheOrDestroy, long numLoops)
+{
+    SEFUtility::CachingFactory<ResettableObject> factory;
+
+    for (int i = 0; i < numLoops; i++)
+    {
+        SEFUtility::CachingFactory<ResettableObject>::UniquePtr object = factory.GetInstance(cacheOrDestroy);
+
+        object->setValue(object->getValue() + 1);
+
+        std::this_thread::sleep_for(std::chrono::microseconds(50));
+    }
+}
+
+constexpr int NUM_THREADS = 200;
+constexpr int NUM_LOOPS = 10000;
+
+TEST_CASE("Test Caching Factory Threading", "[cork-base]")
+{
+    std::array<std::thread, NUM_THREADS> resettableThreads;
+    std::array<std::thread, NUM_THREADS> nonResettableThreads;
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        resettableThreads[i] =
+            std::thread(ResettableThreadMain, SEFUtility::CachingFactoryCacheOrDestroy::CACHE, NUM_LOOPS);
+        nonResettableThreads[i] =
+            std::thread(NonResettableThreadMain, SEFUtility::CachingFactoryCacheOrDestroy::CACHE, NUM_LOOPS);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        resettableThreads[i].join();
+        nonResettableThreads[i].join();
+    }
+
+    REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == NUM_THREADS);
+    REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == NUM_THREADS);
+
+    //	This is a resettable test, so all the objects should be at zero.
+
+    long resettableTotalCount = 0;
+    long nonResettableTotalCount = 0;
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        SEFUtility::CachingFactory<ResettableObject>::UniquePtr resettableObject =
+            SEFUtility::CachingFactory<ResettableObject>::GetInstance(
+                SEFUtility::CachingFactoryCacheOrDestroy::DESTROY);
+        SEFUtility::CachingFactory<NonResettableObject>::UniquePtr nonResettableObject =
+            SEFUtility::CachingFactory<NonResettableObject>::GetInstance(
+                SEFUtility::CachingFactoryCacheOrDestroy::DESTROY);
+
+        resettableTotalCount += resettableObject->getValue();
+        nonResettableTotalCount += nonResettableObject->getValue();
+    }
+
+    REQUIRE(resettableTotalCount == 0);
+    REQUIRE(nonResettableTotalCount == NUM_THREADS * NUM_LOOPS);
+
+    REQUIRE(SEFUtility::CachingFactory<ResettableObject>::numObjectsInCache() == 0);
+    REQUIRE(SEFUtility::CachingFactory<NonResettableObject>::numObjectsInCache() == 0);
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        resettableThreads[i] = std::thread(ResettableThreadMain,
+                                           (i % 2) == 0 ? SEFUtility::CachingFactoryCacheOrDestroy::CACHE
+                                                        : SEFUtility::CachingFactoryCacheOrDestroy::DESTROY,
+                                           10000);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        resettableThreads[i].join();
+    }
+}
---
+++ UnitTest/ConstructOnceResizeableVectorTest.cpp
@@ -0,0 +1,244 @@
+
+#include <catch2/catch_all.hpp>
+
+#include "../CorkLib/Util/ConstuctOnceResizeableVector.h"
+
+//	Resettable element for testing
+
+class TestElement : public SEFUtility::Resettable
+{
+   public:
+    TestElement() : m_value(0), m_reset(true) {}
+
+    virtual ~TestElement() {}
+
+    void reset()
+    {
+        m_value = 0;
+        m_reset = true;
+    }
+
+    bool isReset() const { return (m_reset); }
+
+    void setValue(long newValue)
+    {
+        m_value = newValue;
+        m_reset = false;
+    }
+
+    long getValue() const { return (m_value); }
+
+   private:
+    long m_value;
+    bool m_reset;
+};
+
+//	Run a variety of checks on the size of the vector and insure we cannot overrun vector at its end.
+
+void CheckSize(SEFUtility::ConstructOnceResizeableVector<TestElement>& testVector, size_t correctSize)
+{
+    REQUIRE(testVector.size() == correctSize);
+
+    size_t numElements = 0;
+
+    for (auto& element : testVector)
+    {
+        numElements++;
+    }
+
+    REQUIRE(numElements == correctSize);
+
+    numElements = 0;
+    long runningSum = 0;
+
+    for (size_t i = 0; i < testVector.size(); i++)
+    {
+        numElements++;
+        runningSum += testVector[i].getValue();
+    }
+
+    REQUIRE(numElements == correctSize);
+
+    numElements = 0;
+
+    for (const auto& element : testVector)
+    {
+        numElements++;
+    }
+
+    REQUIRE(numElements == correctSize);
+
+    numElements = 0;
+
+    for (SEFUtility::ConstructOnceResizeableVector<TestElement>::iterator itr = testVector.begin();
+         itr != testVector.end(); itr++)
+    {
+        numElements++;
+    }
+
+    REQUIRE(numElements == correctSize);
+
+    numElements = 0;
+
+    for (SEFUtility::ConstructOnceResizeableVector<TestElement>::const_iterator itr = testVector.begin();
+         itr != testVector.end(); itr++)
+    {
+        numElements++;
+    }
+
+    REQUIRE(numElements == correctSize);
+
+    SEFUtility::ConstructOnceResizeableVector<TestElement>::iterator itr = testVector.end();
+
+    REQUIRE(itr == testVector.end());
+    itr++;
+    REQUIRE(itr++ == testVector.end());
+    REQUIRE(itr++ == testVector.end());
+
+    SEFUtility::ConstructOnceResizeableVector<TestElement>::iterator citr = testVector.end();
+
+    REQUIRE(citr == testVector.end());
+    citr++;
+    REQUIRE(citr++ == testVector.end());
+    REQUIRE(citr++ == testVector.end());
+}
+
+//	Insure all elements of the vector are reset
+
+bool CheckAllReset(const SEFUtility::ConstructOnceResizeableVector<TestElement>& testVector)
+{
+    for (const auto& element : testVector)
+    {
+        if (!element.isReset())
+        {
+            return (false);
+        }
+    }
+
+    return (true);
+}
+
+//	Insure none of the elements of the vector are reset
+
+bool CheckAllNotReset(const SEFUtility::ConstructOnceResizeableVector<TestElement>& testVector)
+{
+    for (const auto& element : testVector)
+    {
+        if (element.isReset())
+        {
+            return (false);
+        }
+    }
+
+    return (true);
+}
+
+//	Compute a sum of all values in the vector
+
+long ComputeRunningSum(const SEFUtility::ConstructOnceResizeableVector<TestElement>& testVector)
+{
+    long runningSum = 0;
+
+    for (const auto& element : testVector)
+    {
+        runningSum += element.getValue();
+    }
+
+    long secondSum = 0;
+
+    for (size_t i = 0; i < testVector.size(); i++)
+    {
+        secondSum += testVector[i].getValue();
+    }
+
+    REQUIRE(secondSum == runningSum);
+
+    return (runningSum);
+}
+
+//	Run a collection of tests to insure the vector runs correctly
+
+TEST_CASE("Construct Once Resizeable Vector Test", "[cork-base]")
+{
+    //	Construct the vector of ten elements and insure it is correctly sized at zero initially
+
+    SEFUtility::ConstructOnceResizeableVector<TestElement> testVector(10);
+
+    REQUIRE(testVector.size() == 0);
+
+    REQUIRE(testVector.reservedSize() == 10);
+
+    //	Resize the vector to 5 - insure it works correctly
+
+    testVector.resize(5);
+
+    REQUIRE(CheckAllReset(testVector));
+
+    CheckSize(testVector, 5);
+
+    long i = 0;
+    long runningSum = 0;
+
+    for (auto& element : testVector)
+    {
+        runningSum += i;
+        element.setValue(i++);
+    }
+
+    REQUIRE(CheckAllNotReset(testVector));
+    REQUIRE(ComputeRunningSum(testVector) == runningSum);
+
+    //	Reset the vector, it shoud be at size zero after it is reset
+
+    testVector.reset();
+
+    CheckSize(testVector, 0);
+
+    //	Reset the vector t oa size of 6
+
+    testVector.resize(6);
+
+    CheckSize(testVector, 6);
+
+    REQUIRE(CheckAllReset(testVector));
+
+    i = 0;
+    runningSum = 0;
+
+    for (auto& element : testVector)
+    {
+        runningSum += i;
+        element.setValue(i++);
+    }
+
+    REQUIRE(CheckAllNotReset(testVector));
+    REQUIRE(ComputeRunningSum(testVector) == runningSum);
+
+    //	Directly resize the vector to a size of 4
+
+    testVector.resize(4);
+
+    CheckSize(testVector, 4);
+
+    REQUIRE(CheckAllReset(testVector));
+
+    //	Resize to a large vector and test
+
+    testVector.resize(123456);
+
+    CheckSize(testVector, 123456);
+
+    REQUIRE(CheckAllReset(testVector));
+
+    i = 0;
+    runningSum = 0;
+
+    for (auto& element : testVector)
+    {
+        runningSum += i;
+        element.setValue(i++);
+    }
+
+    REQUIRE(CheckAllNotReset(testVector));
+    REQUIRE(ComputeRunningSum(testVector) == runningSum);
+}
